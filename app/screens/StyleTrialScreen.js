b9fb64b5c92c0fcdbbb765e477b1a8ca2a81c8d2
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-11-19 04:50:23 +0530

    Text style props

--- app/screens/StyleTrialScreen.js
+++ app/screens/StyleTrialScreen.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import AppScreen from "../components/AppScreen";
 
 function StyleTrialScreen(props) {
+  const f1 = Platform.OS === "android" ? styles.andr1 : styles.ios1;
+  const f2 = Platform.OS === "android" ? styles.andr2 : styles.ios2;
+  const f3 = Platform.OS === "android" ? styles.andr3 : styles.ios3;
+  const test = Platform.OS === "android" ? styles.andrTest : styles.iosTest;
+
   return (
     <AppScreen style={styles.container}>
-      <View style={[styles.outerBox, styles.outerBoxPadding]}>
-        <View style={styles.box}>
-          <Text style={styles.textMargin}>Hi!(margin 20)</Text>
-        </View>
-        <Text>Hi!(padding 20)</Text>
-      </View>
+      <Text style={[styles.text, f1, { color: "red" }]}>First font</Text>
+      <Text style={[styles.text, f2, { color: "green" }]}>Second font</Text>
+      <Text style={[styles.text, f3, { color: "blue" }]}>Third font</Text>
+
+      <Text style={[styles.text, test, { color: "dodgerblue" }]}>
+        First font
+      </Text>
     </AppScreen>
   );
 }
@@ -20,43 +26,25 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  outerBox: {
-    width: 200,
-    height: 200,
-    backgroundColor: "lightgreen",
+  text: {
+    fontSize: 40,
   },
-  box: {
-    width: 100,
-    height: 100,
-    backgroundColor: "dodgerblue",
+  andr1: { fontFamily: "normal" },
+  andr2: { fontFamily: "serif" },
+  andr3: { fontFamily: "monospace" },
+  andrTest: { fontFamily: "Roboto" }, // same as 'normal'
+  //
+  ios1: {
+    fontFamily: "Arial",
   },
-
-  border: {
-    borderWidth: 10,
-    borderColor: "royalblue",
-    borderRadius: 10,
-    borderTopColor: "gold",
+  ios2: {
+    fontFamily: "Georgia",
   },
-
-  shadow: {
-    // ios only
-    shadowColor: "black",
-    shadowOpacity: 0.3,
-    shadowOffset: {
-      width: 10,
-      height: 10,
-    },
-
-    // android only
-    elevation: 20,
+  ios3: {
+    fontFamily: "Georgia",
   },
-
-  outerBoxPadding: {
-    padding: "8%",
-  },
-
-  textMargin: {
-    margin: 10,
+  iosTest: {
+    fontFamily: "Helvetica",
   },
 });
 export default StyleTrialScreen;
