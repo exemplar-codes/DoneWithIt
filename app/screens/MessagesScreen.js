0e252732f742028135dc5cf1d95cc5915a99f6c8
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-11-25 23:40:54 +0530

    Start series 2

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -11,14 +11,15 @@ import {
 const initialMessages = [
   {
     id: 1,
-    title: "T1",
-    description: "D1",
+    title: "Mosh Hamedani",
+    description: "Hey! Is this item still available?",
     image: require("../assets/mosh.jpg"),
   },
   {
     id: 2,
-    title: "T2",
-    description: "D2",
+    title: "Mosh Hamedani",
+    description:
+      "I'm interested in this item. When will you be able to post it?",
     image: require("../assets/mosh.jpg"),
   },
 ];
