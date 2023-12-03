6ee229a96509306756ccc5228ff8fa3afef62a62
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-12-03 17:10:23 +0530

    Add scrollToEnd feature on

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
 import ImageInput from "./ImageInput";
 
 export default function ImageInputList({ images, onAdd, onRemove }) {
+  const ref = useRef();
+  const onAddInternal = (...args) => {
+    onAdd(...args);
+    ref.current.scrollToEnd();
+    // scroll to end so Add icon is always visible by default
+  };
+
   return (
     <View style={styles.container}>
-      <ScrollView horizontal contentContainerStyle={styles.scrollContainer}>
+      <ScrollView
+        ref={ref}
+        horizontal
+        contentContainerStyle={styles.scrollContainer}
+      >
         {images.map((uri) => (
           <ImageInput
             key={uri}
             image={uri}
-            onAdd={onAdd}
+            // onAdd={onAdd}
             onRemove={onRemove}
             showDeleteConfirmation={false}
           />
         ))}
-        <ImageInput key="extra" image="" onAdd={onAdd} />
+        <ImageInput key="extra" image="" onAdd={onAddInternal} />
       </ScrollView>
     </View>
   );
