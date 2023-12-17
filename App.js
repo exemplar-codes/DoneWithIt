fdbfa46a3204563fde6ca3fcbf555736a57f7aee
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-12-17 18:26:06 +0530

    Add splash screen demo

--- App.js
+++ App.js
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
 import LoginScreen from "./app/screens/LoginScreen";
+import * as SplashScreen from "expo-splash-screen";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import { View } from "react-native";
 
 export default function App() {
-  return <LoginScreen />;
+  const [isReady, setIsReady] = useState(false);
+
+  const initializeToken = async (onFinish = () => {}) => {
+    await new Promise((r) => setTimeout(r, 3000));
+    onFinish();
+  };
+
+  useEffect(() => {
+    console.log("useEffect started");
+    initializeToken(() => {
+      console.log("Hiding splash screen");
+      setIsReady(true);
+    });
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
+
+  return <WelcomeScreen />;
 }
